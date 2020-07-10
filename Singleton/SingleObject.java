f8c5ae76efb169d119cb9b2ec9ae8b613f213d4a
Hargi <hargimuhammad@gmail.com>
2020-07-10 14:32:01 +0700

    Singleton

---
+++ Singleton/SingleObject.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hargi
+ */
+public class SingleObject {
+
+   //create an object of SingleObject
+   private static SingleObject instance = new SingleObject();
+
+   //make the constructor private so that this class cannot be
+   //instantiated
+   private SingleObject(){}
+
+   //Get the only object available
+   public static SingleObject getInstance(){
+      return instance;
+   }
+
+   public void showMessage(){
+      System.out.println("Main Menu :");
+      System.out.println("1. Play Game");
+      System.out.println("2. Load Game");
+      System.out.println("3. High Score");
+      System.out.println("4. Exit");
+   }
+}
