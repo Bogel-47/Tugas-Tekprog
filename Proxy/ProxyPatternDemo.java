24eb74e09aebe2cf5a2f6989870561bd7aa53f9c
Hargi <hargimuhammad@gmail.com>
2020-07-10 14:26:23 +0700

    Proxy

---
+++ Proxy/ProxyPatternDemo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hargi
+ */
+public class ProxyPatternDemo {
+	
+   public static void main(String[] args) {
+      Image image = new ProxyImage("191524000.jpg");
+
+      //image will be loaded from disk
+      image.display(); 
+      System.out.println("");
+      
+      //image will not be loaded from disk
+      image.display(); 	
+   }
+}
